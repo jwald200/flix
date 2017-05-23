deaf92e930afffafe3f19505dbc4494a7515892e
jwald1 <email@example.com>
2017-05-23 13:48:50 -0400

    add show action

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,15 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: %i(show)
   def index
     @movies = Movie.all
+  end
+
+  def show
+  end
+
+  private
+
+  def set_movie
+    @movie = Movie.find_by(id: params[:id])
   end
 end
